3a03d256713a0c51d8d9a9994ea4d46ec3d7b0e2
KuroNekoXX <mxnuelkispekxx@gmail.com>
2021-12-09 15:41:59 -0500

    Agregar datasource en UsuarioRepositoryImpl

--- src/main/java/pe/gob/sunarp/pruebas/app/repository/impl/UsuarioRepositoryImpl.java
+++ src/main/java/pe/gob/sunarp/pruebas/app/repository/impl/UsuarioRepositoryImpl.java
@@ -1,16 +1,27 @@
 package pe.gob.sunarp.pruebas.app.repository.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 import pe.gob.sunarp.pruebas.app.mapper.UsuarioModelMapper;
 import pe.gob.sunarp.pruebas.app.model.UsuarioModel;
 import pe.gob.sunarp.pruebas.app.repository.UsuarioRepository;
 
+import javax.sql.DataSource;
+
 @Repository
-public class UsuarioRepositoryImpl implements UsuarioRepository {
+public class UsuarioRepositoryImpl extends JdbcDaoSupport implements UsuarioRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    private ApplicationContext context;
+
+    @Autowired
+    public void DataSource(DataSource setDataSource) {
+        setDataSource(setDataSource);
+        this.jdbcTemplate = context.getBean("db_desa", JdbcTemplate.class);
+    }
 
     @Override
     public UsuarioModel verUsuario(String username) {
